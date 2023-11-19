a6d0b03d9982f6df3d829037ead7b40f1b0b10b0
kismatkc <kismatkc28@gmail.com>
2023-11-19 10:09:27 -0500

    part1 05 006 completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="focaccia pizza"></img>
+      <h2>Pizza spinacci</h2>
+      <p>Tomato,mozoralla,spinich and ricotta cheese</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
